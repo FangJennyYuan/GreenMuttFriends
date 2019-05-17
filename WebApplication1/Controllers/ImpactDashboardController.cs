9eaa40136f712d4517537b272e6e6d70b32d9c7d
Fang Yuan <yuanfang@seattleu.edu>
2019-05-16 23:11:35 -0700

    Re-added correct Impact Dashboard controller

--- WebApplication1/Controllers/ImpactDashboardController.cs
+++ WebApplication1/Controllers/ImpactDashboardController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
 {
-    public class ImpactDashboardController
+    public class ImpactDashboardController : Controller
     {
+        // GET: ImpactDashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
