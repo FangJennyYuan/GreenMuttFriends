b1878c905851b0f4fca078e32fa58cf20cca3cda
Mike Koenig <mikekoenig@live.com>
2019-05-20 15:01:27 -0700

    Added Account Backend, Repository to the project

---
+++ WebApplication1/Backend/AccountBackend.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Backend;
+using WebApplication1.Models;
+
+namespace WebApplication1.Backend
+{
+    public class AccountBackend
+    {
+        #region SingletonPattern
+        private static volatile AccountBackend instance;
+        private static object syncRoot = new object();
+
+        private AccountBackend() { }
+
+        public static AccountBackend Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new AccountBackend();
+                    }
+                }
+
+                return instance;
+            }
+        }
+        #endregion SingletonPattern
+
+        // Hook up the Repositry
+        private IAccountRepository repository = new AccountRepositoryMock();
+
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public AccountModel Create(AccountModel data)
+        {
+            var myData = repository.Create(data);
+            return myData;
+        }
+
+        /// <summary>
+        /// Read
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public AccountModel Read(string id)
+        {
+            var myData = repository.Read(id);
+            return myData;
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public AccountModel Update(AccountModel data)
+        {
+            var myData = repository.Update(data);
+            return myData;
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Delete(string id)
+        {
+            var myData = repository.Delete(id);
+            return myData;
+        }
+
+        /// <summary>
+        ///  Returns the List of Accounts
+        /// </summary>
+        /// <returns></returns>
+        public List<AccountModel> Index()
+        {
+            var myData = repository.Index();
+            return myData;
+        }
+
+        public AccountModel GetActiveUser()
+        {
+            var myData = repository.Index().FirstOrDefault();
+            return myData;
+        }
+    }
+}
---
+++ WebApplication1/Backend/AccountRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Backend
+{
+    /// <summary>
+    /// In Memory Implementation of a Account data store
+    /// </summary>
+    public class AccountRepositoryMock : IAccountRepository
+    {
+        public List<AccountModel> dataset = new List<AccountModel>();
+
+        /// <summary>
+        /// Constructor for Account Repository
+        /// </summary>
+        public AccountRepositoryMock()
+        {
+            // Call for Sead data to be created
+            Initialize();
+        }
+
+        /// <summary>
+        /// Add the log item to the data store
+        /// </summary>
+        /// <param name="data">
+        /// The new log item to add to the data store
+        /// </param>
+        /// <returns>return the passed in log item</returns>
+        public AccountModel Create(AccountModel data)
+        {
+            dataset.Add(data);
+            return data;
+        }
+
+        /// <summary>
+        /// Return the requested log item from the data store
+        /// if not found, return null
+        /// </summary>
+        /// <param name="id">the item to find</param>
+        /// <returns>the item from the datastore, or null</returns>
+        public AccountModel Read(String id)
+        {
+            // Get the first instance of the record
+            var myData = dataset.First(m => m.ID == id);
+            return myData;
+        }
+
+        /// <summary>
+        /// Update the item in the data store
+        /// use the ID from the item passed in to find the item and then update it
+        /// </summary>
+        /// <param name="data">the item to update</param>
+        /// <returns>the updated item</returns>
+        public AccountModel Update(AccountModel data)
+        {
+            // Get the first instance of the record
+            var myData = Read(data.ID);
+            if (myData == null)
+            {
+                return null;
+            }
+
+            myData.Update(data);
+            return data;
+        }
+
+        /// <summary>
+        /// Remove the item from the data store
+        /// Look it up by ID, if found, remove it, and return true
+        /// else return false
+        /// </summary>
+        /// <param name="id">the item to remove by ID</param>
+        /// <returns>true if removed</returns>
+        public Boolean Delete(String id)
+        {
+            // Get the first instance of the record
+            var myData = Read(id);
+            if (myData == null)
+            {
+                return false;
+            }
+
+            var myResult = dataset.Remove(myData);
+            return myResult;
+        }
+
+        /// <summary>
+        /// Return all items in the data store
+        /// </summary>
+        /// <returns>a list of all the items in the data store</returns>
+        public List<AccountModel> Index()
+        {
+            return dataset;
+        }
+
+        /// <summary>
+        /// Sets Initial Seed Data
+        /// </summary>
+        public void Initialize()
+        {
+            // Ijora Clinic data for scenario
+            dataset.Add(new AccountModel { AccountName = "Adaku", isAccountLoggedIn = false });
+
+        }
+    }
+}
