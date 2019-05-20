2903d7c2089b22083a2f4d4bceeee9976b263cf2
Mike Koenig <mikekoenig@live.com>
2019-05-20 14:53:40 -0700

    Added account Model and Account I Account Repository

---
+++ WebApplication1/Backend/IAccountRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Backend
+{
+    /// <summary>
+    /// Define an interface which contains the methods for the Account repository.
+    /// All CRUDi aspects
+    /// </summary>
+    public interface IAccountRepository
+    {
+        AccountModel Create(AccountModel data);
+        AccountModel Read(String id);
+        AccountModel Update(AccountModel data);
+        Boolean Delete(String id);
+        List<AccountModel> Index();
+    }
+}
---
+++ WebApplication1/Models/AccountModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class AccountModel
+    {
+        // The ID of the record
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+
+        // The Account Name
+        public string AccountName { get; set; }
+
+        // Whether the user is logged in or not
+        public bool isAccountLoggedIn { get; set; } = false;
+
+        /// <summary>
+        /// Constructor for Account Model
+        /// Calls to Initialize to set initial settings
+        /// </summary>
+        public AccountModel()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Intialize
+        /// Sets default values, such as DateTime as needed by the system
+        /// </summary>
+        public void Initialize()
+        {
+            // None right now...
+        }
+
+        /// <summary>
+        /// Update fields passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(AccountModel data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            AccountName = data.AccountName;
+            isAccountLoggedIn = data.isAccountLoggedIn;
+
+            return true;
+        }
+    }
+}
