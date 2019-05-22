1f740bc74528bafa99ed2e20a136d5093e7f7cd0
Fang Yuan <yuanfang@seattleu.edu>
2019-05-22 12:16:46 -0700

    Seperated data of App User script from Impact view.

---
+++ WebApplication1/Scripts/AppUserScript.js
@@ -0,0 +1,44 @@
+﻿    var AppUsersdata = [
+        {clinic: "Rawayau Clinic", Date: "05/19/2019", value: 10 },
+        {clinic: "Rawayau Clinic", Date: "05/20/2019", value: 8 },
+        {clinic: "Rawayau Clinic", Date: "05/21/2019", value: 7 },
+        {clinic: "Rawayau Clinic", Date: "05/22/2019", value: 8 },
+        {clinic: "Rawayau Clinic", Date: "05/23/2019", value: 6 },
+        {clinic: "Rawayau Clinic", Date: "05/24/2019", value: 8 },
+        {clinic: "Rawayau Clinic", Date: "05/25/2019", value: 9 },
+
+        {clinic: "Mashegu Clinic", Date: "05/19/2019", value: 12 },
+        {clinic: "Mashegu Clinic", Date: "05/20/2019", value: 4 },
+        {clinic: "Mashegu Clinic", Date: "05/21/2019", value: 8 },
+        {clinic: "Mashegu Clinic", Date: "05/22/2019", value: 8 },
+        {clinic: "Mashegu Clinic", Date: "05/23/2019", value: 7 },
+        {clinic: "Mashegu Clinic", Date: "05/24/2019", value: 9 },
+        {clinic: "Mashegu Clinic", Date: "05/25/2019", value: 8 },
+
+        {clinic: "Katsina Clinic", Date: "05/19/2019", value: 8 },
+        {clinic: "Katsina Clinic", Date: "05/20/2019", value: 9 },
+        {clinic: "Katsina Clinic", Date: "05/21/2019", value: 7 },
+        {clinic: "Katsina Clinic", Date: "05/22/2019", value: 10 },
+        {clinic: "Katsina Clinic", Date: "05/23/2019", value: 8 },
+        {clinic: "Katsina Clinic", Date: "05/24/2019", value: 8 },
+        {clinic: "Katsina Clinic", Date: "05/25/2019", value: 9 },
+
+        {clinic: "Ijowa Clinic", Date: "05/19/2019", value: 8 },
+        {clinic: "Ijowa Clinic", Date: "05/20/2019", value: 10 },
+        {clinic: "Ijowa Clinic", Date: "05/21/2019", value: 11 },
+        {clinic: "Ijowa Clinic", Date: "05/22/2019", value: 11 },
+        {clinic: "Ijowa Clinic", Date: "05/23/2019", value: 12 },
+        {clinic: "Ijowa Clinic", Date: "05/24/2019", value: 14 },
+        {clinic: "Ijowa Clinic", Date: "05/25/2019", value: 15 },
+
+]
+
+var visualization = d3plus.viz()
+    .container("#viz")
+    .data(AppUsersdata)
+    .type("line")
+    .id("clinic")         // key for which our data is unique on
+    .text("clinic")       // key to use for display text
+    .x("Date")
+    .y("value")
+    .draw();
