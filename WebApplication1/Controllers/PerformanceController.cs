f74ac0af018f6d93884ec665c98e5e61ddbf7d5e
Carrie <clipsong@gmail.com>
2019-05-23 14:13:51 -0700

    Aggregated daily photos by clinic in controller and sent to performance view

--- WebApplication1/Controllers/PerformanceController.cs
+++ WebApplication1/Controllers/PerformanceController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using WebApplication1.Models;
 using WebApplication1.Backend;
 
+
 namespace WebApplication1.Controllers
 {
     public class PerformanceController : Controller
@@ -13,10 +14,37 @@ namespace WebApplication1.Controllers
         // GET: Performance
         public ActionResult Index()
         {
-            var myViewModel = new LibraryViewModel();
+            //Model with totals
+            var myViewModel = new List<TotalPhotosTakenModel>();
 
-            myViewModel.PhotoViewModel = PhotoBackend.Instance.Index();
+            //Raw photos from backend
+            var photosRaw = new PhotoViewModel();
+            photosRaw = PhotoBackend.Instance.Index();
 
+
+            //Calculate totals for each clinic on each day
+            var clinicTotals = new Dictionary<string, int>();
+            foreach (var item in photosRaw.PhotoList)
+            {
+                int currentCount = 0;
+                var clinicAndDate = item.ClinicName + "_" + item.RecordedDateTime.ToShortDateString();
+                clinicTotals.TryGetValue(clinicAndDate, out currentCount);
+                clinicTotals[clinicAndDate] = currentCount + 1;
+            }
+
+            //Add aggregate photo totals to model by date and clinic
+            foreach (KeyValuePair<string, int> entry in clinicTotals)
+            {
+                string[] clinicByDate = entry.Key.Split('_');
+                myViewModel.Add(
+                    new TotalPhotosTakenModel
+                    {
+                        Clinic = clinicByDate[0],
+                        Date = DateTime.Parse(clinicByDate[1]),
+                        Value = entry.Value
+                    }
+                );
+            }
             return View(myViewModel);
         }
     }
