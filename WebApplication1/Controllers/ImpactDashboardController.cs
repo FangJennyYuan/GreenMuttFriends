d1f721cfe39086373540c4f828b6579343960f9b
Fang Yuan <yuanfang@seattleu.edu>
2019-05-23 11:08:10 -0700

    Updated Impact Dashboard App User data

--- WebApplication1/Controllers/ImpactDashboardController.cs
+++ WebApplication1/Controllers/ImpactDashboardController.cs
@@ -13,16 +13,38 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             var myData = new List<AppUserDataModel>();
+            myData.Add(new AppUserDataModel { Clinic = "Rawayau Clinic", Date = DateTime.Parse("05/19/2019"), Value = 10 });
+            myData.Add(new AppUserDataModel { Clinic = "Rawayau Clinic", Date = DateTime.Parse("05/20/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel {Clinic = "Rawayau Clinic", Date = DateTime.Parse("05/21/2019"), Value = 7});
+            myData.Add(new AppUserDataModel { Clinic = "Rawayau Clinic", Date = DateTime.Parse("05/22/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Rawayau Clinic", Date = DateTime.Parse("05/23/2019"), Value = 6 });
+            myData.Add(new AppUserDataModel { Clinic = "Rawayau Clinic", Date = DateTime.Parse("05/24/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Rawayau Clinic", Date = DateTime.Parse("05/25/2019"), Value = 9 });
 
-            myData.Add(
-                new AppUserDataModel
-                {
-                    Clinic = "Rawayau Clinic",
-                    Date = DateTime.Parse("05/19/2019"),
-                    Value = 10
-                }
-                );
-            
+            myData.Add(new AppUserDataModel { Clinic = "Mashegu Clinic", Date = DateTime.Parse("05/19/2019"), Value = 12 });
+            myData.Add(new AppUserDataModel { Clinic = "Mashegu Clinic", Date = DateTime.Parse("05/20/2019"), Value = 4 });
+            myData.Add(new AppUserDataModel { Clinic = "Mashegu Clinic", Date = DateTime.Parse("05/21/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Mashegu Clinic", Date = DateTime.Parse("05/22/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Mashegu Clinic", Date = DateTime.Parse("05/23/2019"), Value = 7 });
+            myData.Add(new AppUserDataModel { Clinic = "Mashegu Clinic", Date = DateTime.Parse("05/24/2019"), Value = 9 });
+            myData.Add(new AppUserDataModel { Clinic = "Mashegu Clinic", Date = DateTime.Parse("05/25/2019"), Value = 8 });
+
+            myData.Add(new AppUserDataModel { Clinic = "Katsina Clinic", Date = DateTime.Parse("05/19/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Katsina Clinic", Date = DateTime.Parse("05/20/2019"), Value = 9 });
+            myData.Add(new AppUserDataModel { Clinic = "Katsina Clinic", Date = DateTime.Parse("05/21/2019"), Value = 7 });
+            myData.Add(new AppUserDataModel { Clinic = "Katsina Clinic", Date = DateTime.Parse("05/22/2019"), Value = 10 });
+            myData.Add(new AppUserDataModel { Clinic = "Katsina Clinic", Date = DateTime.Parse("05/23/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Katsina Clinic", Date = DateTime.Parse("05/24/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Katsina Clinic", Date = DateTime.Parse("05/25/2019"), Value = 9 });
+
+            myData.Add(new AppUserDataModel { Clinic = "Ijowa Clinic", Date = DateTime.Parse("05/19/2019"), Value = 8 });
+            myData.Add(new AppUserDataModel { Clinic = "Ijowa Clinic", Date = DateTime.Parse("05/20/2019"), Value = 10 });
+            myData.Add(new AppUserDataModel { Clinic = "Ijowa Clinic", Date = DateTime.Parse("05/21/2019"), Value = 11 });
+            myData.Add(new AppUserDataModel { Clinic = "Ijowa Clinic", Date = DateTime.Parse("05/22/2019"), Value = 11 });
+            myData.Add(new AppUserDataModel { Clinic = "Ijowa Clinic", Date = DateTime.Parse("05/23/2019"), Value = 12 });
+            myData.Add(new AppUserDataModel { Clinic = "Ijowa Clinic", Date = DateTime.Parse("05/24/2019"), Value = 14 });
+            myData.Add(new AppUserDataModel { Clinic = "Ijowa Clinic", Date = DateTime.Parse("05/25/2019"), Value = 15 });
+
             return View(myData);
         }
     }
