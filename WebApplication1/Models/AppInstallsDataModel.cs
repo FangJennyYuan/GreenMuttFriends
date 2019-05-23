2f9fa7c277470634cb30d55416c0d08a3df0e68f
Fang Yuan <yuanfang@seattleu.edu>
2019-05-23 12:58:06 -0700

    Added AppInstallsDataModel.

---
+++ WebApplication1/Models/AppInstallsDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class AppInstallsDataModel
+    {
+        public string Clinic;
+        public DateTime Date;
+        public int Value;
+    }
+}
