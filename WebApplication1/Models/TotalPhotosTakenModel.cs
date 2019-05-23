6a2fe4ca088744604bd092df4a15b9c2f59f25d8
Carrie <clipsong@gmail.com>
2019-05-23 14:02:44 -0700

    Added total photos taken model

---
+++ WebApplication1/Models/TotalPhotosTakenModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class TotalPhotosTakenModel
+    {
+        // The Clinic Name
+        public string Clinic { get; set; }
+
+        // Recorded Date Time photo was taken
+        public DateTime Date { get; set; }
+
+        // The total photos taken
+        public int Value { get; set; }
+    }
+}
