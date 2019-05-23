04c63d8ca2b157724e68b5a626322798240201f3
Carrie <clipsong@gmail.com>
2019-05-22 21:02:23 -0700

    Added call to backend photo data in performance controller

--- WebApplication1/Controllers/PerformanceController.cs
+++ WebApplication1/Controllers/PerformanceController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Backend;
 
 namespace WebApplication1.Controllers
 {
@@ -11,7 +13,11 @@ namespace WebApplication1.Controllers
         // GET: Performance
         public ActionResult Index()
         {
-            return View();
+            var myViewModel = new LibraryViewModel();
+
+            myViewModel.PhotoViewModel = PhotoBackend.Instance.Index();
+
+            return View(myViewModel);
         }
     }
 }
