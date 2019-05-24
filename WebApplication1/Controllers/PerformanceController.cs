7f27633ce3bfeee4b895624ca36bd5d981590e89
Carrie <clipsong@gmail.com>
2019-05-24 09:18:25 -0700

    Calculated total users per day and clinic

--- WebApplication1/Controllers/PerformanceController.cs
+++ WebApplication1/Controllers/PerformanceController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
 using WebApplication1.Backend;
-
+using System.Collections;
 
 namespace WebApplication1.Controllers
 {
@@ -22,26 +22,41 @@ namespace WebApplication1.Controllers
             photosRaw = PhotoBackend.Instance.Index();
 
 
-            //Calculate totals for each clinic on each day
-            var clinicTotals = new Dictionary<string, int>();
+            //Calculate photo totals for each clinic on each day
+            var clinicPhotoTotals = new Dictionary<string, int>();
             foreach (var item in photosRaw.PhotoList)
             {
-                int currentCount = 0;
+                int currentPhotoCount = 0;
                 var clinicAndDate = item.ClinicName + "_" + item.RecordedDateTime.ToShortDateString();
-                clinicTotals.TryGetValue(clinicAndDate, out currentCount);
-                clinicTotals[clinicAndDate] = currentCount + 1;
+                clinicPhotoTotals.TryGetValue(clinicAndDate, out currentPhotoCount);
+                clinicPhotoTotals[clinicAndDate] = currentPhotoCount + 1;
             }
 
             //Add aggregate photo totals to model by date and clinic
-            foreach (KeyValuePair<string, int> entry in clinicTotals)
+            foreach (KeyValuePair<string, int> entry in clinicPhotoTotals)
             {
                 string[] clinicByDate = entry.Key.Split('_');
+                string clinic = clinicByDate[0];
+                string date = clinicByDate[1];
+
+                //Query data to Get the users for this clinic and date
+                IEnumerable<PhotoModel> query = photosRaw.PhotoList.Where(
+                    c => c.ClinicName == clinic
+                    && c.RecordedDateTime.ToShortDateString() == date
+                );
+
+                //Get distinct user count
+                List<string> users  = (from t in query
+                 select t.UserID).Distinct().ToList();
+                int userCount = users.Count;
+
                 myViewModel.Add(
                     new TotalPhotosTakenModel
                     {
-                        Clinic = clinicByDate[0],
-                        Date = DateTime.Parse(clinicByDate[1]),
-                        Value = entry.Value
+                        Clinic = clinic,
+                        Date = DateTime.Parse(date),
+                        Value = entry.Value,
+                        UserCount = userCount
                     }
                 );
             }
