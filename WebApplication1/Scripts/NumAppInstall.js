aaaf6e49350856a5c4d3645d1a3b31a9c12b392f
Carrie <clipsong@gmail.com>
2019-05-23 17:07:52 -0700

    One last file to change for Ijowa to Ijora

--- WebApplication1/Scripts/NumAppInstall.js
+++ WebApplication1/Scripts/NumAppInstall.js
@@ -23,13 +23,13 @@
         {clinic: "Katsina Clinic", Date: "05/24/2019", value: 3 },
         {clinic: "Katsina Clinic", Date: "05/25/2019", value: 4 },
 
-        {clinic: "Ijowa Clinic", Date: "05/19/2019", value: 4 },
-        {clinic: "Ijowa Clinic", Date: "05/20/2019", value: 4 },
-        {clinic: "Ijowa Clinic", Date: "05/21/2019", value: 5 },
-        {clinic: "Ijowa Clinic", Date: "05/22/2019", value: 5 },
-        {clinic: "Ijowa Clinic", Date: "05/23/2019", value: 4 },
-        {clinic: "Ijowa Clinic", Date: "05/24/2019", value: 3 },
-        {clinic: "Ijowa Clinic", Date: "05/25/2019", value: 5 },
+        {clinic: "Ijora Clinic", Date: "05/19/2019", value: 4 },
+        {clinic: "Ijora Clinic", Date: "05/20/2019", value: 4 },
+        {clinic: "Ijora Clinic", Date: "05/21/2019", value: 5 },
+        {clinic: "Ijora Clinic", Date: "05/22/2019", value: 5 },
+        {clinic: "Ijora Clinic", Date: "05/23/2019", value: 4 },
+        {clinic: "Ijora Clinic", Date: "05/24/2019", value: 3 },
+        {clinic: "Ijora Clinic", Date: "05/25/2019", value: 5 },
 
 ]
 
