9650713d02be31cb0d225746017b6fcb4cae0241
Fang Yuan <yuanfang@seattleu.edu>
2019-05-24 19:15:37 -0700

    Created new AppInstallGraph script to clean up Impact page.

---
+++ WebApplication1/Scripts/umd/AppInstallGraph.js
@@ -0,0 +1,47 @@
+﻿function drawAppUsersGraph(NumAppInstallData) {
+    var attributes = [
+        { "clinic": "Ijora Clinic", "hex": "#5FBD73" },
+        { "clinic": "Katsina Clinic", "hex": "#6A5599" },
+        { "clinic": "Mashegu Clinic", "hex": "#71BDD3" },
+        { "clinic": "Rawayau Clinic", "hex": "#1C2C8C" }
+    ]
+
+    var visualization = d3plus.viz()
+        .container("#viz1")
+        .data({
+            "value": NumAppInstallData,
+            "stroke": { "width": 3 }
+
+        })
+        .type("line")
+        .id("clinic")         // key for which our data is unique on
+        .text("clinic")       // key to use for display text
+        .x({
+            "value": "Date",
+            "grid": { "color": "#ffffff" }
+        })
+        .y("value")
+        .attrs(attributes)
+        .color("hex")
+        .font({
+            "family": "'Century Gothic', Helvetica, Arial, sans-serif",
+            "size": 15
+        })
+        .axes({
+            "background": {
+                "color": "#ffffff",
+                "stroke": { "width": 0 }
+            }
+        })
+        .format({
+            "text": function (text, params) {
+                if (text === "value") {
+                    return "Total Installs";
+                } else {
+                    return d3plus.string.title(text, params);
+                }
+            }
+        })
+        .height(600)
+        .draw()
+}
