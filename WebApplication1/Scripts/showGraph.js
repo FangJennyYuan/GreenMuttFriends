b76d09eed1da9c0f22e5e48c891c193f358e4d17
Fang Yuan <yuanfang@seattleu.edu>
2019-05-27 21:16:20 -0700

    Added script to show tables on fav click.

---
+++ WebApplication1/Scripts/showGraph.js
@@ -0,0 +1,12 @@
+﻿function showGraph() {
+    var checkBox = document.getElementById("id-of-input1");
+    // Get the output text
+    var graph = document.getElementById("image");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+        graph.style.display = "block";
+    } else {
+        graph.style.display = "none";
+    }
+}
