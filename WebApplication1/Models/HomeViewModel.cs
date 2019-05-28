2acc76612d2b8d711e2d976fd3ecac9fa1075beb
Fang Yuan <yuanfang@seattleu.edu>
2019-05-28 12:56:24 -0700

    Created HomeViewModel page.

---
+++ WebApplication1/Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class HomeViewModel
+    {
+        // List of User information
+        public UserViewModel UserViewModel { get; set; }
+
+        // List of Photo information
+        public PhotoViewModel PhotoViewModel { get; set; }
+    }
+}
