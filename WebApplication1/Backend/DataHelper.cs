86974c0d51e209ffc20c8ea6a14040669e023f38
Carrie <clipsong@gmail.com>
2019-06-04 21:23:34 -0700

    Create helper class for calculating total photos

---
+++ WebApplication1/Backend/DataHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Backend
+{
+    public static class DataHelper
+    {
+
+        public static int TotalPhotosByDayAndClinic(PhotoViewModel data)
+        {
+            var totalPhotosByDay = data.PhotoList.GroupBy(m => new { m.ClinicName, m.RecordedDateTime.DayOfYear })
+                    .Select(group => new
+                    {
+                        ClinicName = group.Key,
+                        Count = group.Count()
+
+                    })
+                    .ToList();
+            return 0;
+            
+        }
+    }
+}
