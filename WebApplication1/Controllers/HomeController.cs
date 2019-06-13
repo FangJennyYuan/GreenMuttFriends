9b2d68bb52bf6c9a7ca9ab5562da0fafb3c40252
cbrod <broderickcel@seattleu.edu>
2019-06-13 10:06:17 -0700

    Removing unused "About" template page

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,13 +18,6 @@ namespace WebApplication1.Controllers
             return View(mymodel);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
